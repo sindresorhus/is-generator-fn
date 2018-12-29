e7b2dde906f065a086e7a8f8e9f1d5838a4b4316
Sindre Sorhus <sindresorhus@gmail.com>
2018-12-29 16:15:38 +0100

    Update index.d.ts

--- index.d.ts
+++ index.d.ts
@@ -1,7 +1,4 @@
 /**
  * Check if something is a generator function.
- *
- * @param fn - The value to check.
- * @returns Returns true if fn is generator function, else false.
  */
-export default function isGeneratorFn (fn: any): fn is GeneratorFunction;
+export default function isGeneratorFn(value: unknown): value is GeneratorFunction;
